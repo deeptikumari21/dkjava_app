ecb67e3bbb9694538fbd49ba9ef8c3b6ebd84615
deeptikumari21 <deepti25kumari@gmail.com>
2025-07-14 21:10:08 +0530

    CI test update

--- src/main/java/com/lab/App.java
+++ src/main/java/com/lab/App.java
@@ -2,6 +2,6 @@ package com.lab;
  
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins - Demo Java App!");
+       System.out.println("CI Triggered Automatically!");
     }
 }
